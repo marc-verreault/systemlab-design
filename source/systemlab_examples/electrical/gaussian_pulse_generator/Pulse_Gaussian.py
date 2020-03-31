bf80aaccf625f0af4fe9517dbcd4067da4aa4585
Marc Verreault <marc.verreault@systemlabdesign.com>
2020-03-31 10:16:07 -0400

    Release 20.01.r2 updates

--- source/systemlab_examples/electrical/gaussian_pulse_generator/Pulse_Gaussian.py
+++ source/systemlab_examples/electrical/gaussian_pulse_generator/Pulse_Gaussian.py
@@ -1,9 +1,9 @@
 """
-SystemLab-Design Version 19.02
+SystemLab-Design Version 19.12.r1 
 
 SCRIPT TEMPLATE FOR FUNCTIONAL BLOCKS
-Version: 1.0
-Date: 5-March-2019
+Version: 2.0
+Date: 20-Dec-2019
 """
 
 import numpy as np
@@ -34,76 +34,85 @@ def run(input_signal_data, parameters_input, settings):
     if config.sim_status_win_enabled == True:
         config.sim_status_win.textEdit.append('Starting ' + module_name + 
                                           ' - Iteration #: ' + str(iteration))
-    # Data output (when enabled) - sent to Data window
+    config.status.setText('Running ' + module_name + 
+                                          ' - Iteration #: ' + str(iteration))
+    config.app.processEvents()
+
     if config.sim_data_activate == True:
         config.sim_data_view.dataEdit.append('Data output for ' + module_name + 
                                           ' - Iteration #: ' + str(iteration))                                     
                                           
-    '''==INPUT PARAMETERS=============================================================='''
-    # Load parameters from FB parameters table [row][column]
-    # Parameter name(0), Value(1), Units(2), Notes(3)
-    # e.g. par_1 = float(parameters_input[0][1])
-    # e.g. par_2 = str(parameters_input[1][1])
+    '''==INPUT PARAMETERS=======================================================
+    Load parameters from FB parameters table [row][column]
+    Parameter name(0), Value(1), Units(2), Notes(3)
+    '''
+    #par_1 = float(parameters_input[0][1])
+    #par_2 = str(parameters_input[1][1])
+    #Additional parameters (local)
+    #par_3 = 'abc'
+    #par_4 = 100
 
-    # Additional parameters (local)
-#    par_3 = 'abc'
-#    par_4 = 100
-    
-    '''==INPUT SIGNALS====================================================================
+    '''==INPUT SIGNALS==========================================================
+    Digital: portID(0), signal_type(1), symbol_rate(2), bit_rate(3), order(4), time_array(5), discrete_array(6)
+    Electrical: portID(0), signal_type(1), carrier(2), sample_rate(3), time_array(4), amplitude_array(5), noise_array(6)
+    Optical_signal: portID(0), sig_type(1), fs(2), time_array(3), psd_array(4), optical_group(5)
+    Optical_channel(s): wave_key(0), wave_freq(1), jones_vector(2), e_field_array(3), noise_array(4)
+    For further info see:
+    https://systemlabdesign.com/syslab_documentation/_build/html/syslab_documents/SignalsLibrary.html
+    Note: If there is no input signal, output settings must be locally declared
     '''
-    # input_signal_data = [[signal_port_a], [signal_port_b], ...]
-    # Digital: portID(0), signal_type(1), symbol_rate(2), bit_rate(3), order(4),
-    # time_array(5), discrete_array(6)
-    # Electrical: portID(0), signal_type(1), carrier(2), sample_rate(3), time_array(4), 
-    # amplitude_array(5), noise_array(6)
-    # Optical collection: [portID(0), signal_type(1), sample_rate(2), time_array(3), 
-    # optical_channel_list(4)]
-    # Optical_channel_list:
-    # [[wave_key(0), wave_channel(1), jones_matrix(2), envelope_array(3), noise_array(4),
-    # psd_array(5)], [wave_key(0), wave_channel(1), jones_matrix(2), envelope_array(3),
-    # noise_array(4), psd_array(5)],...]
-    # input_data_1 = input_signal_data[a][b], where a is list index and b is element index
-    # for list a
     
-    '''==CALCULATIONS=====================================================================
+    '''==CALCULATIONS===========================================================
     '''
-    #config.sim_status_win.textEdit.append('Data: ' + str(input_data
-        
     carrier = 0
-    sig_type_out = 'Electrical'
+    sig_type = 'Electrical'
     time_array = np.linspace(0, time, n)
     sig_array = signal.gausspulse(time_array, fc=10)
     noise_array = np.zeros(n)
-        
-    '''==OUTPUT PARAMETERS LIST===========================================================
+    
+    '''==OUTPUT PARAMETERS LIST===================================================
     '''
     script_parameters = []
     script_parameters = parameters_input #If NO changes are made to parameters
-#    par_1 = ['Par_1', value, 'units', 'Description']   
-#    script_parameters = [par_1, par_2]
+    #par_1 = ['Par_1', value, 'units', 'Description']   
+    #script_parameters = [par_1, par_2]
   
-    '''==RESULTS==========================================================================
+    '''==RESULTS========================================================
+    Results are returned and loaded into 'Output data table' tab of functional block. 
+    For further info see:
+    https://systemlabdesign.com/syslab_documentation/_build/html/syslab_documents/AideMemoire.html
     '''
     script_results = []
-#    result_1 = ['Name_1', value_1, 'units_1', 'Description_1']
-#    result_2 = ['Name_2', value_2, 'units_2', 'Description_2']
-#    script_results = [result_1, result_2]
+    #result_1 = ['Name_1', value_1, 'units_1', 'Description_1', False, 'format']
+    #result_2 = ['Name_2', value_2, 'units_2', 'Description_2', False, 'format']]
+    #script_results = [result_1, result_2]
     
-    '''==DATA PANEL=======================================================================
+    '''==DATA PANEL(S)=====================================================
+    Used to export data to Data panel viewers (optional)
+    For further info see:
+    https://systemlabdesign.com/syslab_documentation/_build/html/syslab_documents/AideMemoire.html
+    config.data_tables['data_table_1'] = [] # 'Data_table_1' is linked to the data field Data source file name
+    data_1 = ['Data name 1', data_value_1, 'format 1', 'units 1', 'color name 1', 'color data 1']
+    data_2 = ['Data name 2', data_value_2, 'format 2', 'units 2', 'color name 2', 'color data 2']
+    data_list = [data_1, data_2]
+    config.data_tables['data_table_1'] .extend(data_list) # Add data lists to table list
     '''
-    #Send update to data panel (data_table_1)
-#    data_1 = ['Name_1', value_1, 'units_1']
-#    data_2 = ['Name_2', value_2, 'units_2']
-#    config.data_table_1.append(data_1)
-#    config.data_table_2.append(data_2)    
 
-
-    '''==RETURN (Output Signals, Parameters, Results)=================================='''
-# DIGITAL  
-#      digital_out = [1, signal_type, symbol_rate, bit_rate, order, time_array, binary_array]
-# ELECTRICAL        
-    electrical_out = [1, sig_type_out, carrier, fs, time_array, sig_array, noise_array]
-# OPTICAL
-#      optical_group = [[wave_key, wave_freq, jones_vector, e_field_array, noise_array, psd_array]]
-#      optical_out = [3, signal_type, fs, time_array, optical_group]
+    '''==RETURN (Output Signals, Parameters, Results)==================================
+    '''
+    #DIGITAL  
+    #digital_out = [1, signal_type, symbol_rate, bit_rate, order, time_array, digital_output]
+    #return ([digital_out], script_parameters, script_results)
+    
+    #ELECTRICAL        
+    electrical_out = [1, sig_type, carrier, fs, time_array, sig_array, noise_array]   
     return ([electrical_out], script_parameters, script_results)
+    
+    #OPTICAL
+    #optical_channels = []
+    #for ch in range(0, channels):
+    #    opt_ch = [int(wave_key[ch]), wave_freq[ch], jones_vector[ch], 
+    #    opt_field_out[ch], noise_field_out[ch]]
+    #    optical_channels.append(opt_ch)
+    #return ([[1, signal_type, fs, time_array, psd_array, optical_channels]], 
+    #              script_parameters, script_results)
